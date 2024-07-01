3807a90cc71fe482338fd9bde156c98050442e14
stateside2 <ianedwardgrant@gmail.com>
2024-07-01 18:40:18 +0100

    Update 017 Sort fix

--- HOME.py
+++ HOME.py
@@ -38,32 +38,40 @@ st.divider()
 # st.write("Week 16 - 23/4/2024")
 File_Date = "Week 24 - 12th June 2024"
 
-# --- MENU NAVBAR --- (failed to work on Safari)
-# menu_selection = option_menu(menu_title=None,
-# 	options=["League Table", "Goals", "MOTM", "Board Room"],
-#	icons=["trophy-fill", "life-preserver", "person-arms-up", "bank"],
-#	default_index=0,
-#	orientation="horizontal")
 
 # --- MENU NAVBAR ---
-menu_selection = sac.segmented(
-    items=[
-        sac.SegmentedItem(label="League Table", icon="trophy-fill"),
-        sac.SegmentedItem(label="Goals", icon="life-preserver"),
-        sac.SegmentedItem(label="MOTM", icon="person-arms-up"),
-        sac.SegmentedItem(label="Board Room", icon="bank"),
+#menu_selection = sac.segmented(
+#    items=[
+#        sac.SegmentedItem(label="League Table", icon="trophy-fill"),
+#       sac.SegmentedItem(label="Goals", icon="life-preserver"),
+#        sac.SegmentedItem(label="MOTM", icon="person-arms-up"),
+#        sac.SegmentedItem(label="Board Room", icon="bank"),
         #sac.SegmentedItem(label="Board Room", icon='share-fill', href='https://mantine.dev/core/segmented-control/'),
-    ], label=File_Date, format_func=None, align="center", size="md", radius="md", color="dark", bg_color=None, divider=False, use_container_width=True
-)
+#    ], label=File_Date, format_func=None, align="center", size="md", radius="md", color="dark", bg_color=None, divider=False, use_container_width=True
+#)
+
+# --- MENU NAVBAR --- 
+menu_selection = sac.buttons(
+   items=[
+    	sac.ButtonsItem(label="League Table", icon="trophy-fill"),
+    	sac.ButtonsItem(label="Goals", icon="life-preserver"),
+    	sac.ButtonsItem(label="MOTM", icon="person-arms-up"),
+    	sac.ButtonsItem(label="Board Room", icon="bank"),
+], label=File_Date, format_func=None, align="center", size="md", radius="md", color="#4682b4", use_container_width=True)
+# ---
+
 
 # --- PANDAS DATA FRAME SELECTION ---
 df_ltable = pd.read_excel(excel_file, skiprows=[0,1,3,39,40], sheet_name='League Table', usecols=[0,52,54,58,59])
-df_goals = pd.read_excel(excel_file, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
-df_broom = pd.read_excel(excel_file, skiprows=7, nrows=19, sheet_name='Board Room', usecols=[12,13]).fillna(0)
-df_motm = pd.read_excel(excel_file, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
 
-# --- MAKES THE BOARD ROOM PLAYER COLUMN UPPER CASE ---
-df_broom["Unnamed: 12"] = df_broom["Unnamed: 12"].str.upper()
+df_goals = pd.read_excel(excel_file, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
+df_goals = df_goals.sort_values(by=["TOTAL", "PLAYER"], ascending=[False, True])
+
+df_motm = pd.read_excel(excel_file, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
+df_motm = df_motm.sort_values(by=["VOTES", "PLAYER"], ascending=[False, True])
+
+df_broom = pd.read_excel(excel_file, skiprows=7, nrows=19, sheet_name='Board Room', usecols=[12,13]).fillna(0)
+df_broom["Unnamed: 12"] = df_broom["Unnamed: 12"].str.upper() # --- MAKES THE BOARD ROOM PLAYER COLUMN UPPER CASE ---
 df_broom = df_broom.sort_values(by=["Unnamed: 13", "Unnamed: 12"], ascending=[False, True])
 
 
@@ -71,10 +79,10 @@ if menu_selection == "League Table":
 	st.dataframe(df_ltable, width=None, height=1275, use_container_width=True, hide_index=True, column_order=["POSITION","PLAYER","PLAYED","G/D","PTS"], column_config={"POSITION": " ", "PLAYED": "P", "G/D": "GD", "PTS": "Pts"})
 if menu_selection == "Goals":
 	st.dataframe(df_goals, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"TOTAL": "GOALS"})
-if menu_selection == "Board Room":
-	st.dataframe(df_broom, width=None, height=750, use_container_width=True, hide_index=True, column_config={"Unnamed: 12": "PLAYER", "Unnamed: 13": "VISITS"})
 if menu_selection == "MOTM":
 	st.dataframe(df_motm, width=None, height=1225, use_container_width=True, hide_index=True)
+if menu_selection == "Board Room":
+	st.dataframe(df_broom, width=None, height=750, use_container_width=True, hide_index=True, column_config={"Unnamed: 12": "PLAYER", "Unnamed: 13": "VISITS"})
 
 
 st.divider()
---
+++ pages/SHOP.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(page_title="Shop", page_icon="images/marlowdukesicon.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# --- HIDE 1) STREAMLIT HEADER/FOOTER MENUS, 2) POP-UP DOWNLOAD, SEARCH, EXPAND DATAFRAME ELEMENTS, 3) EXPAND IMAGE  ---
+hide_st_style = """ 
+	<style>MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;} 
+	[data-testid="stElementToolbar"] {display: none;} 
+	button[title="View fullscreen"] {visibility: hidden;} 
+	</style>
+	"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+# ----
+
+
+one, two, three = st.columns([0.2,0.6,0.2])
+two.image("images/shop_offsidegarbbannerlight.png", width=400)
+
+#st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://offside-garb.myspreadshop.co.uk", use_container_width=True)
+st.button(":grey[COMING SOON!!]", use_container_width=True)
+
+left, middle, right = st.columns(3)
+left.image("images/shop_marlowdukesbaseballcap.png", caption="Contrast Snapback Cap")
+middle.image("images/shop_marlowdukestshirt.png", caption="Mens T-shirt")
+right.image("images/shop_marlowdukesbuckethat.png", caption="Bucket Hat")
+
+#st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://offside-garb.myspreadshop.co.uk/", use_container_width=True)
+
+
