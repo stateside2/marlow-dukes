54b962998dac6a06c912d42783b1804f4da1f972
stateside2 <ianedwardgrant@gmail.com>
2024-07-02 12:36:30 +0100

    SHOP OPEN

--- pages/SHOP.py
+++ pages/SHOP.py
@@ -13,17 +13,19 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # ----
 
 
-one, two, three = st.columns([0.2,0.6,0.2])
-two.image("images/shop_offsidegarbbannerlight.png", width=400)
+#one, two, three = st.columns([0.2,0.6,0.2])
+#two.image("images/shop_offsidegarbbannerlight.png", width=400)
 
-#st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://offside-garb.myspreadshop.co.uk", use_container_width=True)
-st.button(":grey[COMING SOON!!]", use_container_width=True)
+st.image("images/shop_marlowmerchbanner.png", use_column_width=True)
+
+st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://marlow-merch.myspreadshop.co.uk", use_container_width=True)
+#st.button(":grey[COMING SOON!!]", use_container_width=True)
 
 left, middle, right = st.columns(3)
 left.image("images/shop_marlowdukesbaseballcap.png", caption="Contrast Snapback Cap")
 middle.image("images/shop_marlowdukestshirt.png", caption="Mens T-shirt")
 right.image("images/shop_marlowdukesbuckethat.png", caption="Bucket Hat")
 
-#st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://offside-garb.myspreadshop.co.uk/", use_container_width=True)
+st.link_button(":grey[CLICK HERE TO VISIT THE STORE]", url="https://marlow-merch.myspreadshop.co.uk/", use_container_width=True)
 
 
