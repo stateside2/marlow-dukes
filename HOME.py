1353c228686d27f032def9edb99d769e908b2b60
stateside2 <ianedwardgrant@gmail.com>
2024-08-13 12:27:59 +0100

    Update 023 v1.3 All time stats

--- HOME.py
+++ HOME.py
@@ -1,17 +1,14 @@
 
-# in terminal > pip install pandas openpyxl streamlit streamlit-option-menu
-# pip install pipreqs ... installs whats needed to createt the reqirements.txt
-# excel sheet names = League Table, Captains, MOTM, Goals, Board Room
-# icons from bootstrap
 
-# import os
-# import time
 import pandas as pd
 import streamlit as st
-# from streamlit_option_menu import option_menu
 import streamlit_antd_components as sac #--- NEEDED FOR THE 2ND NAVBAR
+import time # --- USED IN THE miles_notif NOTIFICATION FUNCTION
 
-excel_file = "data/latest_data.xlsx"
+excel_file_season: str = "data/latest_data.xlsx"
+excel_file_hof: str = "data/hall_of_fame.xlsx" # --- NEEDED FOR THE miles_notif NOTIFICATION FUNCTION
+
+
 st.set_page_config(page_title="Marlow Dukes", page_icon="images/marlowdukesicon.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 # --- HIDE STREAMLIT HEADER/FOOTER MENUS ---
@@ -26,29 +23,13 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # --- BANNER IMAGE
 st.image("images/marlowdukesbanner.png", use_column_width="auto")
+#st.success("Congratulation to the 2023 WINNERS!!", icon=None)
+#st.success("League: xxx, Goals: xxx, MOTM: xxx, Board Room: xxx", icon=None)
 st.divider()
 
-# --- EXCEL SOURCE FILE DATE ---
-# file_totalsecs = os.stat(excel_file).st_mtime
-# file_year = str(time.gmtime(file_totalsecs).tm_year)
-# file_month = str(time.gmtime(file_totalsecs).tm_mon)
-# file_day = str(time.gmtime(file_totalsecs).tm_mday)
-# st.write(file_day,"/",file_month,"/",file_year)
-
 # st.write("Week 16 - 23/4/2024")
-File_Date = "Week 30 - 24th July 2024"
+File_Date = "Week 32 - 7th August 2024"
 
-
-# --- MENU NAVBAR ---
-#menu_selection = sac.segmented(
-#    items=[
-#        sac.SegmentedItem(label="League Table", icon="trophy-fill"),
-#       sac.SegmentedItem(label="Goals", icon="life-preserver"),
-#        sac.SegmentedItem(label="MOTM", icon="person-arms-up"),
-#        sac.SegmentedItem(label="Board Room", icon="bank"),
-        #sac.SegmentedItem(label="Board Room", icon='share-fill', href='https://mantine.dev/core/segmented-control/'),
-#    ], label=File_Date, format_func=None, align="center", size="md", radius="md", color="dark", bg_color=None, divider=False, use_container_width=True
-#)
 
 # --- MENU NAVBAR --- 
 menu_selection = sac.buttons(
@@ -62,21 +43,53 @@ menu_selection = sac.buttons(
 
 
 # --- PANDAS DATA FRAME SELECTION ---
-df_ltable = pd.read_excel(excel_file, skiprows=[0,1,3,39,40], sheet_name='League Table', usecols=[0,52,54,58,59])
+df_ltable = pd.read_excel(excel_file_season, skiprows=[0,1,3,39,40], sheet_name='League Table', usecols=[0,52,54,58,59])
 
-df_goals = pd.read_excel(excel_file, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
+df_goals = pd.read_excel(excel_file_season, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
 df_goals = df_goals.sort_values(by=["TOTAL", "PLAYER"], ascending=[False, True])
 
-df_motm = pd.read_excel(excel_file, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
+df_motm = pd.read_excel(excel_file_season, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
 df_motm = df_motm.sort_values(by=["VOTES", "PLAYER"], ascending=[False, True])
 
-df_broom = pd.read_excel(excel_file, skiprows=7, nrows=19, sheet_name='Board Room', usecols=[12,13]).fillna(0)
+df_broom = pd.read_excel(excel_file_season, skiprows=7, nrows=19, sheet_name='Board Room', usecols=[12,13]).fillna(0)
 df_broom["Unnamed: 12"] = df_broom["Unnamed: 12"].str.upper() # --- MAKES THE BOARD ROOM PLAYER COLUMN UPPER CASE ---
 df_broom = df_broom.sort_values(by=["Unnamed: 13", "Unnamed: 12"], ascending=[False, True])
 
 
+# ---  MENU SELECTION AND DF DISPLAY
 if menu_selection == "League Table":
 	st.dataframe(df_ltable, width=None, height=1275, use_container_width=True, hide_index=True, column_order=["POSITION","PLAYER","PLAYED","G/D","PTS"], column_config={"POSITION": " ", "PLAYED": "P", "G/D": "GD", "PTS": "Pts"})
+
+	# --- MILESTONE NOTIFICATION FUNCTION ---
+	def miles_notif(col_metric: str) -> str:
+		# --- ALL-TIME DATAFRAME BUILD FOR NOTIFICATION FUNCTION
+		miss_rows: list[int] = [0,1,3,39,40]
+		df_season_full_tab = pd.read_excel(excel_file_season, skiprows=miss_rows, sheet_name='League Table', usecols=[0,54,55,56,57,58,59]) # --- DROP POSITION COLUMN
+		df_hof_full_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,3,4,5,6,7])
+		df_at_full_tab = df_season_full_tab.join(df_hof_full_tab.set_index("PLAYER"), on="PLAYER", how="outer", lsuffix="_season", rsuffix="_hof")
+		df_at_full_tab = df_at_full_tab.fillna(0)
+		df_at_full_tab["PLAYED"] = (df_at_full_tab["PLAYED_season"]+df_at_full_tab["PLAYED_hof"])
+		df_at_full_tab["WON"] = (df_at_full_tab["WON_season"]+df_at_full_tab["WON_hof"])
+		df_at_full_tab["LOST"] = (df_at_full_tab["LOST_season"]+df_at_full_tab["LOST_hof"])
+		# ---
+		# --- PLAYER AND MESSAGE NOTIFICATION
+		milestones: list[int] = range(50, 601, 50)
+		mst_name: str = df_at_full_tab[df_at_full_tab[col_metric].isin(milestones)]["PLAYER"].tolist()
+		number: int = df_at_full_tab[df_at_full_tab["PLAYER"].isin(mst_name)][col_metric].tolist()
+		for mst_name, number in zip(mst_name, number):
+			if col_metric == "PLAYED":
+				st.toast(f"Congratulations, {mst_name} ... {number:.0f} all-time appearances!", icon="⭐")
+				time.sleep(2)
+			if col_metric == "LOST":
+				st.toast(f"UH-OH {mst_name} ... {number:.0f} all-time losses.", icon="❗")
+				time.sleep(2)
+			if col_metric == "WON":
+				st.toast(f"Congratulations, {mst_name} ... {number:.0f} all-time wins!", icon="🏅")
+
+	miles_notif(col_metric = "PLAYED")
+	miles_notif(col_metric = "LOST")
+	miles_notif(col_metric = "WON")
+
 if menu_selection == "Goals":
 	st.dataframe(df_goals, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"TOTAL": "GOALS"})
 if menu_selection == "MOTM":
@@ -87,17 +100,13 @@ if menu_selection == "Board Room":
 
 st.divider()
 
-
-# --- HALL OF FAME LINK NEEDED ---
-
 # --- FUTURE ADDITIONS
 # st.markdown("##")
 # st.snow() # --- WINTER / XMAS UPDATE --- DOES IT WORK ON CELLPHONE?
 # st.balloons() # --- END OF SEASON CELEBRATION --- DOES IT WORK ON CELLPHONE?
 # st.toast("xxxxxxx is the winner", icon="🔥") # --- WINNER ANNOUNCEMENT --- DOES IT WORK ON CELLPHONE?
 # time.sleep(1) --- ALSO HAVE TO import time
+# POSSIBLE ICONS = [🔥, 🚨, 💩, 💥, 🔆, 😎, 😖, 😟, 🥇, 🏅, ☠️, ⚠️, ⚽, ⭐, 💯, ✅, ❗, 🏆]
 
 # st.title("Title Marlow Dukes") --- st.header("This is a header") --- st.subheader("Sub Header") --- st.markdown("data correct as of ...")
 
-# col_list=df_broom.columns
-# st.markdown(col_list)
--- pages/HALL_OF_FAME.py
+++ pages/HALL_OF_FAME.py
@@ -1,114 +1,141 @@
 
-# import os #--- NEEDED FOR THE FILE DATE CALCULATION
-# import time
 import pandas as pd
 import streamlit as st
-# from streamlit_option_menu import option_menu #--- NEEDED FOR THE FIRST NAVBAR
-import streamlit_antd_components as sac #--- NEEDED FOR THE 2ND NAVBAR
+import streamlit_antd_components as sac
 
 
-excel_file = "data/hall_of_fame.xlsx"
-st.set_page_config(page_title="Hall Of Fame", page_icon="images/marlowdukesicon.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
+excel_file_hof: str = "data/hall_of_fame.xlsx"
+excel_file_season: str = "data/latest_data.xlsx"
 
-# --- HIDE STREAMLIT HEADER/FOOTER MENUS ---
+
+st.set_page_config(page_title="Marlow Dukes", page_icon="images/marlowdukesicon.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# --- HIDE 1) STREAMLIT HEADER/FOOTER MENUS, 2) POP-UP DOWNLOAD, SEARCH, EXPAND DATAFRAME ELEMENTS, 3) EXPAND IMAGE  ---
 hide_st_style = """ 
 	<style>MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;} 
 	[data-testid="stElementToolbar"] {display: none;} 
-	button[title="View fullscreen"] {visibility: hidden;}
+	button[title="View fullscreen"] {visibility: hidden;} 
 	</style>
 	"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
 # ----
 
+
 # --- BANNER IMAGE
 st.image("images/marlowdukesbannerHOF.png", use_column_width="auto")
 st.divider()
 
-
-# --- MENU NAVBAR --- 
+# --- MENU BUTTONS
 hof_selection = sac.buttons(
    items=[
-    sac.ButtonsItem(label="Champions", icon="award"),
-    sac.ButtonsItem(label="Podiums", icon="list-ol"),
-], label=None, format_func=None, align="center", size="md", radius="md", color="#4682b4", use_container_width=True)
+    sac.ButtonsItem(label="All-time League Table", icon="bar-chart-fill"),
+    sac.ButtonsItem(label="All-time Appearances", icon="bar-chart-fill"),
+    sac.ButtonsItem(label="All-time Wins", icon=None),
+    sac.ButtonsItem(label="All-time Draws", icon=None),
+    sac.ButtonsItem(label="All-time Losses", icon="bar-chart-fill"),
+    sac.ButtonsItem(label="All-time Goal Difference", icon=None),
+    sac.ButtonsItem(label="All-time Win Ratio", icon=None),
+    sac.ButtonsItem(label="All-time Loss Ratio", icon=None),
+    sac.ButtonsItem(label="All-time Points per Match", icon=None),
+    sac.ButtonsItem(label="Previous Champions", icon=None),
+    sac.ButtonsItem(label="Previous Podiums", icon=None),
+], label=None, format_func=None, align="center", size="xs", radius="2", color="#4682b4", use_container_width=True)
+
+
+# --- CHAMPIONS DF
+df_hof_champs = pd.read_excel(excel_file_hof, sheet_name="CHAMPIONS", skiprows=None, usecols=[0,1,2,6])
+df_hof_champs["SEASON"] = df_hof_champs["SEASON"].astype(str) #--- CONVERTS 2,023 TO 2023
+
+
+# --- ALL-TIME TABLE BUILD FUNCTION
+def df_at_full_tab_build() -> None:
+	miss_rows: list[int] = [0,1,3,39,40]
+	df_season_full_tab = pd.read_excel(excel_file_season, skiprows=miss_rows, sheet_name='League Table', usecols=[0,54,55,56,57,58,59]) # --- DROP POSITION COLUMN
+	df_hof_full_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,3,4,5,6,7])
+	df_at_full_tab = df_season_full_tab.join(df_hof_full_tab.set_index("PLAYER"), on="PLAYER", how="outer", lsuffix="_season", rsuffix="_hof")
+
+	df_at_full_tab = df_at_full_tab.fillna(0)
+	df_at_full_tab["PLAYED"] = (df_at_full_tab["PLAYED_season"]+df_at_full_tab["PLAYED_hof"])
+	df_at_full_tab["WON"] = (df_at_full_tab["WON_season"]+df_at_full_tab["WON_hof"])
+	df_at_full_tab["DRAWN"] = (df_at_full_tab["DRAWN_season"]+df_at_full_tab["DRAWN_hof"])
+	df_at_full_tab["LOST"] = (df_at_full_tab["LOST_season"]+df_at_full_tab["LOST_hof"])
+	df_at_full_tab["Pts"] = (df_at_full_tab["PTS_season"]+df_at_full_tab["PTS_hof"])
+	df_at_full_tab["GD"] = (df_at_full_tab["G/D_season"]+df_at_full_tab["G/D_hof"])
+	df_at_full_tab["WIN RATIO"] = (df_at_full_tab["WON"]/df_at_full_tab["PLAYED"])
+	df_at_full_tab["LOSS RATIO"] = (df_at_full_tab["LOST"]/df_at_full_tab["PLAYED"])
+	df_at_full_tab["PTS/MATCH"] = (df_at_full_tab["Pts"]/df_at_full_tab["PLAYED"])
+	return df_at_full_tab
+# ----
+df_at_full_tab = df_at_full_tab_build()
+
+
+# --- RACE CHART LINK FUNCTION ---
+def race_chart(button_name: str, target_link: str) -> None:
+	sac.buttons(
+   	items=[
+    		sac.ButtonsItem(label=button_name, icon="bar-chart-fill", href=target_link),
+    		], label=None, format_func=None, align="center", variant="filled", size="sm", radius="2", color="#4682b4", use_container_width=False)
+# ---
+
+# --- PODIUM PAGE TABLES BUILD FUNCTION ---
+def podium_table_build(skip_row_list: list[int]) -> None:
+	df_hof_podium = pd.read_excel(excel_file_hof, sheet_name="TOP3", skiprows=skip_row_list, nrows=3, usecols=[0,1,2,6,7])
+	st.dataframe(df_hof_podium, width=None, height=142, use_container_width=True, hide_index=True, column_config={"PLAYED": "P", "POINTS": "Pts"})
+# ---
+
+# --- RATIO TABLE BUILD FUNCTION ---
+def ratio_table_build(ratio: str, new_col_name: str) -> None:
+	global df_at_full_tab
+	df_at_full_tab = df_at_full_tab.sort_values(by=[ratio, "PLAYER"], ascending=[False, True])
+	if ratio in ["WIN RATIO", "LOSS RATIO", "PTS/MATCH"]:
+		df_at_full_tab = df_at_full_tab.style.format({ratio: "{:.3f}"})
+	st.dataframe(df_at_full_tab, width=None, height=2050, use_container_width=True, hide_index=True, column_order=["PLAYER",ratio], column_config={ratio: new_col_name})
 # ---
 
 
-# --- PANDAS DATA FRAME SELECTION ---
-if hof_selection == "Champions":
-	hof_champs = pd.read_excel(excel_file, sheet_name="CHAMPIONS", skiprows=None, usecols=[0,1,2,6])
-	hof_champs["SEASON"] = hof_champs["SEASON"].astype(str) #--- CONVERTS 2,023 TO 2023
-	st.dataframe(hof_champs, width=None, height=458, use_container_width=True, hide_index=True, column_order=["SEASON","CHAMPION","PLAYED","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+# --- PANDAS DATA FRAME SELECTION & DISPLAY ---
 
-if hof_selection == "Podiums":
+if hof_selection == "All-time League Table":
+	race_chart("2012-23 POINTS RACE CHART", "https://public.flourish.studio/visualisation/18936682/")
+	df_at_full_tab = df_at_full_tab.sort_values(by=["Pts", "GD"], ascending=[False, False])
+	st.dataframe(df_at_full_tab, width=None, height=2050, use_container_width=True, hide_index=True, column_order=["PLAYER","PLAYED","WON","DRAWN","LOST","GD","Pts"], column_config={"PLAYER": " ", "PLAYED": "P", "WON": "W", "DRAWN": "D", "LOST": "L"})
+	
+if hof_selection == "All-time Appearances":
+	race_chart("2012-23 APPEARANCES RACE CHART", "https://public.flourish.studio/visualisation/18900148/")
+	ratio_table_build("PLAYED", "APPEARANCES")
+		
+if hof_selection == "All-time Wins":
+	ratio_table_build("WON", "WINS")
+	
+if hof_selection == "All-time Draws":
+	ratio_table_build("DRAWN", "DRAWS")
+	
+if hof_selection == "All-time Losses":
+	race_chart("2012-23 BIGGEST LOSER RACE CHART", "https://public.flourish.studio/visualisation/18921440/")
+	ratio_table_build("LOST", "LOSSES")
 
-	hof_2023 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=None, nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2023, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2023","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+if hof_selection == "All-time Goal Difference":
+	ratio_table_build("GD", "GD")
 
-	hof_2022 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2022, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2022","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+if hof_selection == "All-time Win Ratio":
+	ratio_table_build("WIN RATIO", "WIN RATIO")
 
-	hof_2021 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2021, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2021","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+if hof_selection == "All-time Loss Ratio":
+	ratio_table_build("LOSS RATIO", "LOSS RATIO")
 
-	hof_2020 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2020, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2020","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+if hof_selection == "All-time Points per Match":
+	ratio_table_build("PTS/MATCH", "PTS/MATCH")
 
-	hof_2019 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2019, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2019","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
+if hof_selection == "Previous Champions":
+	st.dataframe(df_hof_champs, width=None, height=458, use_container_width=True, hide_index=True, column_order=["SEASON","CHAMPION","PLAYED","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
 
-	hof_2018 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2018, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2018","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2017 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2017, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2017","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2016 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2016, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2016","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2015 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2015, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2015","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2014 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2014, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2014","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2013 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2013, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2013","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-	hof_2012 = pd.read_excel(excel_file, sheet_name="TOP3", skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43], nrows=3, usecols=[0,1,2,6,7])
-	st.dataframe(hof_2012, width=None, height=142, use_container_width=True, hide_index=True, column_order=["2012","PLAYER","PLAYED","GD","POINTS"], column_config={"PLAYED": "P", "POINTS": "Pts"})
-
-
-# --- PANDAS DATA FRAME SELECTION ---
-# df_ltable = pd.read_excel(excel_file, skiprows=[0,1,3,37,38], sheet_name='League Table', usecols=[0,52,54,58,59])
-# df_goals = pd.read_excel(excel_file, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
-# df_broom = pd.read_excel(excel_file, skiprows=7, sheet_name='Board Room', usecols=[12,13])
-# df_motm = pd.read_excel(excel_file, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
-
-
-# if menu_selection == "League Table":
-# 	st.dataframe(df_ltable, width=None, height=1225, use_container_width=True, hide_index=True, column_order=["POSITION","PLAYER","PLAYED","G/D","PTS"], column_config={"POSITION": " ", "PLAYED": "P", "G/D": "GD", "PTS": "Pts"})
-# if menu_selection == "Goals":
-# 	st.dataframe(df_goals, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"TOTAL": "GOALS"})
-# if menu_selection == "Board Room":
-# 	st.dataframe(df_broom, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"Unnamed: 12": "PLAYER", "Unnamed: 13": "VISITS"})
-# if menu_selection == "MOTM":
-# 	st.dataframe(df_motm, width=None, height=1225, use_container_width=True, hide_index=True)
-
+if hof_selection == "Previous Podiums":
+	podium_table_build(None)
+	season: list[int] = range(2022,2011,-1) # --- NEED TO MODIFY FOR A NEW SEASON
+	skip_row_list_max: int = 4
+	for i in season:
+		podium_table_build(range(0,skip_row_list_max))
+		skip_row_list_max: int = skip_row_list_max + 4
 
 st.divider()
 
-
-# --- HALL OF FAME LINK NEEDED ---
-
-# --- FUTURE ADDITIONS
-# st.markdown("##")
-# st.snow() # --- WINTER / XMAS UPDATE --- DOES IT WORK ON CELLPHONE?
-# st.balloons() # --- END OF SEASON CELEBRATION --- DOES IT WORK ON CELLPHONE?
-# st.toast("xxxxxxx is the winner", icon="🔥") # --- WINNER ANNOUNCEMENT --- DOES IT WORK ON CELLPHONE?
-# time.sleep(1) --- ALSO HAVE TO import time
-
-# st.title("Title Marlow Dukes") --- st.header("This is a header") --- st.subheader("Sub Header") --- st.markdown("data correct as of ...")
-
-# col_list=df_broom.columns
-# st.markdown(col_list)
