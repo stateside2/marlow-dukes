24e23d11c144975d2b90c9e652fbd616f49c7f0a
stateside2 <ianedwardgrant@gmail.com>
2024-09-11 16:26:57 +0100

    AT Goals MOTM add

--- pages/HALL_OF_FAME.py
+++ pages/HALL_OF_FAME.py
@@ -29,6 +29,8 @@ st.divider()
 hof_selection = sac.buttons(
    items=[
     sac.ButtonsItem(label="All-time League Table", icon="bar-chart-fill"),
+    sac.ButtonsItem(label="All-time Goals", icon="bar-chart-fill"), #--- 1.4
+    sac.ButtonsItem(label="All-time MOTM Votes", icon="bar-chart-fill"), #--- 1.4
     sac.ButtonsItem(label="All-time Appearances", icon="bar-chart-fill"),
     sac.ButtonsItem(label="All-time Wins", icon=None),
     sac.ButtonsItem(label="All-time Draws", icon=None),
@@ -51,7 +53,7 @@ df_hof_champs["SEASON"] = df_hof_champs["SEASON"].astype(str) #--- CONVERTS 2,02
 def df_at_full_tab_build() -> None:
 	miss_rows: list[int] = [0,1,3,39,40]
 	df_season_full_tab = pd.read_excel(excel_file_season, skiprows=miss_rows, sheet_name='League Table', usecols=[0,54,55,56,57,58,59]) # --- DROP POSITION COLUMN
-	df_hof_full_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,3,4,5,6,7])
+	df_hof_full_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,3,4,5,6,7,8,9])
 	df_at_full_tab = df_season_full_tab.join(df_hof_full_tab.set_index("PLAYER"), on="PLAYER", how="outer", lsuffix="_season", rsuffix="_hof")
 
 	df_at_full_tab = df_at_full_tab.fillna(0)
@@ -68,6 +70,29 @@ def df_at_full_tab_build() -> None:
 # ----
 df_at_full_tab = df_at_full_tab_build()
 
+#--- 1.4
+def df_at_var_tab_build(sheet_name: str) -> None:
+	if sheet_name == "Goals":
+		df_season_goals_tab = pd.read_excel(excel_file_season, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
+		df_hof_goals_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,8])
+		df_at_goals_tab = df_season_goals_tab.join(df_hof_goals_tab.set_index("PLAYER"), on="PLAYER", how="outer", lsuffix="_season", rsuffix="_hof")
+		
+		df_at_goals_tab = df_at_goals_tab.fillna(0)
+		df_at_goals_tab["ALL-TIME GOALS"] = (df_at_goals_tab["TOTAL"]+df_at_goals_tab["GOALS"])
+		return df_at_goals_tab
+
+	elif sheet_name == "MOTM":
+		df_season_MOTM_tab = pd.read_excel(excel_file_season, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
+		df_hof_MOTM_tab = pd.read_excel(excel_file_hof, sheet_name="ALL TIME TABLE", skiprows=[0], usecols=[1,2,9])
+		df_at_MOTM_tab = df_season_MOTM_tab.join(df_hof_MOTM_tab.set_index("PLAYER"), on="PLAYER", how="outer", lsuffix="_season", rsuffix="_hof")
+
+		df_at_MOTM_tab = df_at_MOTM_tab.fillna(0)
+		df_at_MOTM_tab["ALL-TIME VOTES"] = (df_at_MOTM_tab["VOTES"]+df_at_MOTM_tab["MOTM"])
+		return df_at_MOTM_tab
+# ---
+df_at_goals_tab = df_at_var_tab_build("Goals")
+df_at_MOTM_tab = df_at_var_tab_build("MOTM")
+# ---
 
 # --- RACE CHART LINK FUNCTION ---
 def race_chart(button_name: str, target_link: str) -> None:
@@ -100,6 +125,16 @@ if hof_selection == "All-time League Table":
 	df_at_full_tab = df_at_full_tab.sort_values(by=["Pts", "GD"], ascending=[False, False])
 	st.dataframe(df_at_full_tab, width=None, height=2050, use_container_width=True, hide_index=True, column_order=["PLAYER","PLAYED","WON","DRAWN","LOST","GD","Pts"], column_config={"PLAYER": " ", "PLAYED": "P", "WON": "W", "DRAWN": "D", "LOST": "L"})
 	
+if hof_selection == "All-time Goals": #--- 1.4
+	race_chart("2012-23 GOALS RACE CHART", "https://public.flourish.studio/visualisation/19245477/")
+	df_at_goals_tab = df_at_goals_tab.sort_values(by=["ALL-TIME GOALS", "PLAYER"], ascending=[False, False])
+	st.dataframe(df_at_goals_tab, width=None, height=2100, use_container_width=True, hide_index=True, column_order=["PLAYER","ALL-TIME GOALS"], column_config={"ALL-TIME GOALS": "GOALS"})
+
+if hof_selection == "All-time MOTM Votes": #--- 1.4
+	race_chart("2012-23 MOTM RACE CHART", "https://public.flourish.studio/visualisation/19244438/")
+	df_at_MOTM_tab = df_at_MOTM_tab.sort_values(by=["ALL-TIME VOTES", "PLAYER"], ascending=[False, False])
+	st.dataframe(df_at_MOTM_tab, width=None, height=2050, use_container_width=True, hide_index=True, column_order=["PLAYER","ALL-TIME VOTES"], column_config={"ALL-TIME VOTES": "VOTES"})
+
 if hof_selection == "All-time Appearances":
 	race_chart("2012-23 APPEARANCES RACE CHART", "https://public.flourish.studio/visualisation/18900148/")
 	ratio_table_build("PLAYED", "APPEARANCES")
