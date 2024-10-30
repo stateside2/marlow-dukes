c369d3944efb4719400e1dbccd21c7fa71c30fcb
stateside2 <ianedwardgrant@gmail.com>
2024-10-30 16:52:01 +0000

    039 Week 44 latest data

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 43
+game_week = 44
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 43 - 23rd October 2024"
+File_Date = "Week 44 - 30th October 2024"
