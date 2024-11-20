95764376a393b2fa9d922a8fa2a8ecf682d2af9c
stateside2 <ianedwardgrant@gmail.com>
2024-11-20 21:31:29 +0000

    041 Week 47 Latest Data

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 45
+game_week = 47
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 45 - 6th November 2024"
+File_Date = "Week 47 - 20th November 2024"
