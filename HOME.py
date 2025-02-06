1ee5a65b1ae7c1843ce3944f178a3bc1dbae518b
stateside2 <ianedwardgrant@gmail.com>
2025-02-06 13:15:51 +0000

    051 Week 5 update

--- HOME.py
+++ HOME.py
@@ -43,27 +43,27 @@ menu_selection = sac.buttons(
 # --- HOME/LEAGUE TABLE DF BUILD
 
 # --- WEEK5UPDATE --- REMOVED UNTIL WEEK 5
-# formguide_home_cols=[0,52,54,58,59,(game_week-4),(game_week-3),(game_week-2),(game_week-1),game_week] #--- THIS WILL BREAK WHEN GAME_WEEK < 5
+formguide_home_cols=[0,50,51,55,56,(game_week-4),(game_week-3),(game_week-2),(game_week-1),game_week] #--- THIS WILL BREAK WHEN GAME_WEEK < 5
 
-formguide_home_cols=[0,50,51,55,56]
+# formguide_home_cols=[0,50,51,55,56]
 df_ltable = pd.read_excel(excel_file_season, skiprows=[0,1,3,40,41], sheet_name='League Table', usecols=formguide_home_cols)
 
 # --- WEEK5UPDATE --- REMOVED UNTIL WEEK 5
-# def form_guide_league(game_week):
-# 	i = game_week - 4
-# 	match = 5
-# 	while i <= game_week:
-# 		df_ltable.loc[df_ltable["WK "+str(i)] > 0, "WK-"+str(match)] = "🟢"
-# 		df_ltable.loc[df_ltable["WK "+str(i)] < 0, "WK-"+str(match)] = "🔴"
-# 		df_ltable.loc[df_ltable["WK "+str(i)] == 0, "WK-"+str(match)] = "⚪"
-# 		df_ltable.loc[df_ltable["WK "+str(i)].isnull(), "WK-"+str(match)] = "➖"
-# 		i = i + 1
-# 		match = match - 1
-# 	return
+def form_guide_league(game_week):
+	i = game_week - 4
+	match = 5
+	while i <= game_week:
+		df_ltable.loc[df_ltable["WK "+str(i)] > 0, "WK-"+str(match)] = "🟢"
+		df_ltable.loc[df_ltable["WK "+str(i)] < 0, "WK-"+str(match)] = "🔴"
+		df_ltable.loc[df_ltable["WK "+str(i)] == 0, "WK-"+str(match)] = "⚪"
+		df_ltable.loc[df_ltable["WK "+str(i)].isnull(), "WK-"+str(match)] = "➖"
+		i = i + 1
+		match = match - 1
+	return
 	
-# form_guide_league(game_week)
+form_guide_league(game_week)
 
-# df_ltable["FORM"] = df_ltable["WK-1"]+"  "+df_ltable["WK-2"]+df_ltable["WK-3"]+df_ltable["WK-4"]+df_ltable["WK-5"]
+df_ltable["FORM"] = df_ltable["WK-1"]+"  "+df_ltable["WK-2"]+df_ltable["WK-3"]+df_ltable["WK-4"]+df_ltable["WK-5"]
 # ----
 
 # ----
@@ -132,29 +132,44 @@ if menu_selection == "League Table":
 
 
 # ---- GOALS DF BUILD
-df_goals = pd.read_excel(excel_file_season, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=[0,52])
+goal_guide_cols=[0,52,(game_week-4),(game_week-3),(game_week-2),(game_week-1),game_week]
+df_goals = pd.read_excel(excel_file_season, skiprows=[0,1,3,37,38,39,40], sheet_name='Goals', usecols=goal_guide_cols)
+
+def goal_form_guide(game_week):
+	i = game_week - 4
+	match = 5
+	while i <= game_week:
+		df_goals.loc[df_goals["WK "+str(i)] > 0, "WK-"+str(match)] = "⚽"
+		df_goals.loc[df_goals["WK "+str(i)].isnull(), "WK-"+str(match)] = "➖"
+		i = i + 1
+		match = match - 1
+	return
+goal_form_guide(game_week)
+
+df_goals["FORM"] = df_goals["WK-1"]+df_goals["WK-2"]+df_goals["WK-3"]+df_goals["WK-4"]+df_goals["WK-5"]
+
 df_goals = df_goals.sort_values(by=["TOTAL", "PLAYER"], ascending=[False, True])
 df_goals.insert(0, "POSITION", range(1, 1 + len(df_goals)))
 
+
 # ---- MOTM DF BUILD
-df_motm = pd.read_excel(excel_file_season, skiprows=[1,35,36,37,38,39], sheet_name='MOTM', usecols=[0,52])
+df_motm = pd.read_excel(excel_file_season, skiprows=[1,36,37,38,39,40], sheet_name='MOTM', usecols=[0,52])
 df_motm = df_motm.sort_values(by=["VOTES", "PLAYER"], ascending=[False, True])
 df_motm.insert(0, "POSITION", range(1, 1 + len(df_motm)))
 
 # ---- BOARDROOM DF BUILD
-df_broom = pd.read_excel(excel_file_season, skiprows=7, nrows=22, sheet_name='Board Room', usecols=[12,13]).fillna(0)
+df_broom = pd.read_excel(excel_file_season, skiprows=7, nrows=7, sheet_name='Board Room', usecols=[12,13]).fillna(0)
 df_broom["Unnamed: 12"] = df_broom["Unnamed: 12"].str.upper() # --- MAKES THE BOARD ROOM PLAYER COLUMN UPPER CASE ---
 df_broom = df_broom.sort_values(by=["Unnamed: 13", "Unnamed: 12"], ascending=[False, True])
 df_broom.insert(0, "POSITION", range(1, 1 + len(df_broom)))
 
 
 if menu_selection == "Goals":
-	st.dataframe(df_goals, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"POSITION": " ","TOTAL": "GOALS"})
-
+	st.dataframe(df_goals, width=None, height=1225, use_container_width=True, hide_index=True, column_order=["POSITION","PLAYER","TOTAL","FORM"], column_config={"POSITION": " ","TOTAL": "GOALS"})
 if menu_selection == "MOTM":
-	st.dataframe(df_motm, width=None, height=1225, use_container_width=True, hide_index=True, column_config={"POSITION": " "})
+	st.dataframe(df_motm, width=None, height=1230, use_container_width=True, hide_index=True, column_config={"POSITION": " "})
 if menu_selection == "Board Room":
-	st.dataframe(df_broom, width=None, height=185, use_container_width=True, hide_index=True, column_config={"POSITION": " ","Unnamed: 12": "PLAYER", "Unnamed: 13": "VISITS"})
+	st.dataframe(df_broom, width=None, height=360, use_container_width=True, hide_index=True, column_config={"POSITION": " ","Unnamed: 12": "PLAYER", "Unnamed: 13": "VISITS"})
 
 st.divider()
 
--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 4
+game_week = 5
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 4 - 28th January 2025"
+File_Date = "Week 5 - 5th February 2025"
