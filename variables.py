9c9fad36a393a1fea23b4cf9876567478e97c2da
stateside2 <ianedwardgrant@gmail.com>
2025-03-22 19:22:28 +0000

    055 Week 11 Update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 9
+game_week = 11
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 9 - 5th March 2025"
+File_Date = "Week 11 - 19th March 2025"
