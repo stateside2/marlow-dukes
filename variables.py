5783b37c15bc4695c3bbbe4ea72fa15f90c0707b
stateside2 <ianedwardgrant@gmail.com>
2025-04-16 18:03:38 +0100

    060 Game week 15 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 14
+game_week = 15
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 14 - 9th April 2025"
+File_Date = "Week 15 - 16th April 2025"
