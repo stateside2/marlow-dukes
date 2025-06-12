d75506f894529065356871890fb4f03207865da4
stateside2 <ianedwardgrant@gmail.com>
2025-06-12 22:25:12 +0100

    067 Game week 23 udpate

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 21
+game_week = 23
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 21 - 28th May 2025"
+File_Date = "Week 23 - 11th June 2025"
