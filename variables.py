abcabdd4ad7f35ecbb24581baf2173d72002a57d
stateside2 <ianedwardgrant@gmail.com>
2025-07-09 17:50:49 +0100

    072 Game week 27 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 26
+game_week = 27
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 26 - 2nd July 2025"
+File_Date = "Week 27 - 9th July 2025"
