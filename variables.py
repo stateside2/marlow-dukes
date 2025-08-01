afcafd3d8b716a359d3b2249ecfc5de5e0a64e7f
stateside2 <ianedwardgrant@gmail.com>
2025-08-01 12:26:22 +0100

    Game week 30 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 29
+game_week = 30
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 29 - 23rd July 2025"
+File_Date = "Week 30 - 30th July 2025"
