bc5bd68392eb916d9809ede09bc4e1e586716175
stateside2 <ianedwardgrant@gmail.com>
2025-11-03 14:59:04 -0500

    089 Game week 43 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 42
+game_week = 43
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 42 - 22nd October 2025"
+File_Date = "Week 43 - 29th October 2025"
