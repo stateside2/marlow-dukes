a84d98f92b3f4b93d359da7bdbc32531835fd1c1
stateside2 <ianedwardgrant@gmail.com>
2026-03-11 18:57:01 +0000

    098 Game week 10 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 8
+game_week = 10
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 8 - 25th February"
+File_Date = "Week 10 - 11th March 2026"
