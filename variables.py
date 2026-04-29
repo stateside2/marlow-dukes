de52d0c85f3aa5d24358e7acbbc1d82e4a9cea41
stateside2 <ianedwardgrant@gmail.com>
2026-04-29 18:19:41 +0100

    105 Game week 17 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 16
+game_week = 17
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 16 - 23rd April 2026"
+File_Date = "Week 17 - 29th April 2026"
