33fad648a8c2f406d27837734e921bc97eb103d2
stateside2 <ianedwardgrant@gmail.com>
2026-06-04 17:51:34 +0100

    106 Game week 22 update

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 # ---- USED FOR FORM GUIDE FUNCTION IN HOME.py and 6_STATISTICS.py
-game_week = 17
+game_week = 22
 
 # ---- USED AS MENU TITLE IN HOME.py
-File_Date = "Week 17 - 29th April 2026"
+File_Date = "Week 22 - 3rd June 2026"
